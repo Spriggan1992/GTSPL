3d400e59ba1250a6ca943decac3188f1de175a9b
romankozlov <spriggan1992@gmail.com>
2023-10-31 23:00:47 +0700

    add amont of print6

--- android/src/main/kotlin/pro/unger1984/gtspl/GtsplPlugin.kt
+++ android/src/main/kotlin/pro/unger1984/gtspl/GtsplPlugin.kt
@@ -60,7 +60,6 @@ class GtsplPlugin: FlutterPlugin, MethodCallHandler {
       "send_command" -> {
         val arguments = call.arguments as HashMap<*,*>
         if (arguments.containsKey("command")) {
-           mGtsplWIFICmdTest.GTSPL_printLabel(2, 3, context)
           val template = arguments["command"] as String
           val res = mGtsplWIFICmdTest.sendToPrinter("QTY 3\n$template");
           result.success(res);
