d1f3fd408228d81dea9ec0b6257f26258bc241e4
romankozlov <spriggan1992@gmail.com>
2023-11-02 20:14:25 +0700

    add amont of print7

--- android/src/main/kotlin/pro/unger1984/gtspl/GtsplPlugin.kt
+++ android/src/main/kotlin/pro/unger1984/gtspl/GtsplPlugin.kt
@@ -60,9 +60,8 @@ class GtsplPlugin: FlutterPlugin, MethodCallHandler {
       "send_command" -> {
         val arguments = call.arguments as HashMap<*,*>
         if (arguments.containsKey("command")) {
-           mGtsplWIFICmdTest.GTSPL_printLabel(1, 2, context)
           val template = arguments["command"] as String
-          val res = mGtsplWIFICmdTest.sendToPrinter("QTY 3\n$template");
+          val res = mGtsplWIFICmdTest.sendToPrinter("QTY 2\n$template");
           result.success(res);
         }else {
           result.error("invalid_argument", "argument 'command' not found", null)
